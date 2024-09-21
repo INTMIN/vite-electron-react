cc28af9b9e112856271e065f4c3ded2877f23097
INTMIN <min_word@qq.com>
2024-09-21 18:25:18 +0800

    ts类以及函数笔记

--- src/example.ts
+++ src/example.ts
@@ -22,7 +22,7 @@ enum TextObj{
 }
 
 /**
- * @description  不会有返回值的被称为never
+ * @description  不会执行完的函数才能写为never,否则只是不会有返回值的被称为void
  * @author min
  * @date 21/09/2024
  * @return {*}  {never}
@@ -48,3 +48,49 @@ let newArr:Array<string|number>
 // readonly 不能给数组的泛型一起使用 下面的写法会报错
 // let readArr:readonly Array<string|number>=[1,2]
 let readArr:readonly number[] = [1,2]
+
+// 设置只有问号可以省略
+function f(x?: number | undefined) {
+  return x;
+}
+
+
+// 直接定义会报错
+// class Point {
+//   a:number;
+//   b:string
+// }
+
+// 但是这么写不会，表示内部属性肯定有
+class Point {
+  a!:number;
+  b!:string
+}
+
+
+//类的检查条件以及限制
+interface Country {
+  name: string;
+  capital?: string;
+}
+// 或者
+// type Country = {
+//   name: string;
+//   capital: string;
+// };
+class MyCountry implements Country {
+  name = "";
+  capital = "";
+}
+class newCountry{
+  name!:string
+}
+
+class oldCountry{
+  capital!:string
+}
+
+// class多个接口
+class Country implements MyCountry, newCountry, oldCountry {
+  // ...
+}
